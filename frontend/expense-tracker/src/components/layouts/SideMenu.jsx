06aff7231a1a67d5203cc4b35c803645f1fe53bd
psharma1280 <116288664+psharma1280@users.noreply.github.com>
2025-06-15 01:44:52 +0530

    SideMenu and connecting APIs

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -1,10 +1,72 @@
-    import React from 'react'
-    import {SIDE_MENU_DATA} from "../../utils/data";
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SIDE_MENU_DATA } from '../../utils/data';
+import { UserContext } from '../../context/UserContext';
+import CharAvatar from '../Cards/CharAvatar'; 
 
-    const SideMenu = () => {
-    return (
-        <div>SideMenu</div>
-    )
-}
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
 
-    export default SideMenu
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if(route==="logout"){
+        handleLogout;
+        return;
+    }
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
+  return (
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
+      {/* Profile Section */}
+      <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
+        {user?.profileImageUrl ? (
+          <img
+            src={user.profileImageUrl || ""}
+            alt="Profile Image"
+            className="w-20 h-20 bg-slate-400 rounded-full object-cover"
+          />
+        ) : (
+          <CharAvatar
+            fullName={user?.fullName || ""}
+            width="w-20"
+            height="h-20"
+            style="text-xl"
+          />
+        )}
+        <h5 className="text-gray-950 font-semibold text-[16px] leading-5 capitalize">
+          {user?.fullName || ""}
+        </h5>
+      </div>
+
+      {/* Menu Buttons */}
+      {SIDE_MENU_DATA.map((item, index) => (
+        <button
+          key={`menu_${index}`}
+          type="button"
+          className={`w-full flex items-center gap-4 text-[15px] py-3 px-3 rounded-lg mb-3 
+            ${activeMenu === item.label ? "text-white bg-primary" : "text-black hover:bg-gray-200"}`}
+          onClick={() => {
+            if (item.isLogout) {
+              handleLogout();
+            } else {
+              handleClick(item.path);
+            }
+          }}
+        >
+          <item.icon className="text-xl" />
+          {item.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default SideMenu;
