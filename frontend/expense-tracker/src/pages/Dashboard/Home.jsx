aba6eef65846d70a1ba5993821cb034f595a8d05
psharma1280 <116288664+psharma1280@users.noreply.github.com>
2025-06-15 18:27:41 +0530

    TransactionPage
    created the transaction files

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,57 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import DashboardLayout from "../../components/layouts/DashboardLayout";
-import { useUserAuth } from "../../hooks/useUserAuth";
-import axiosInstance from "../../utils/axiosInstance";
-import { API_PATHS } from "../../utils/apiPaths";
-import InfoCard from "../../components/Cards/InfoCard";
+  import React, { useState, useEffect } from 'react';
+  import { useNavigate } from 'react-router-dom';
+  import DashboardLayout from "../../components/layouts/DashboardLayout";
+  import { useUserAuth } from "../../hooks/useUserAuth";
+  import axiosInstance from "../../utils/axiosInstance";
+  import { API_PATHS } from "../../utils/apiPaths";
+  import InfoCard from "../../components/Cards/InfoCard";
+  import RecentTransactions from "../../components/Dashboard/RecentTransactions"; 
 
-import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
-import { IoMdCard } from "react-icons/io";
-import { addThousandsSeperator } from "../../utils/helper";
 
-const Home = () => {
-  useUserAuth();
+  import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+  import { IoMdCard } from "react-icons/io";
+  import { addThousandsSeperator } from "../../utils/helper";
 
-  const navigate = useNavigate();
-  const [dashboardData, setDashboardData] = useState(null);  
-  const [loading, setLoading] = useState(false);
+  const Home = () => {
+    useUserAuth();
 
-  const fetchDashboardData = async () => {
-    if (loading) return;
+    const navigate = useNavigate();
+    const [dashboardData, setDashboardData] = useState(null);  
+    const [loading, setLoading] = useState(false);
 
-    try {
-      setLoading(true);
-      const response = await axiosInstance.get(API_PATHS.DASHBOARD.GET_DATA);
-      if (response.data) {
-        setDashboardData(response.data);
+    const fetchDashboardData = async () => {
+      if (loading) return;
+
+      try {
+        setLoading(true);
+        const response = await axiosInstance.get(API_PATHS.DASHBOARD.GET_DATA);
+        if (response.data) {
+          setDashboardData(response.data);
+        }
+      } catch (error) {
+        console.log("Something went wrong. Please try again", error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.log("Something went wrong. Please try again", error);
-    } finally {
-      setLoading(false);
-    }
+    };
+
+    useEffect(() => {
+      fetchDashboardData();
+      return () => {};
+    }, []);
+
+    return (
+      <DashboardLayout activeMenu="Dashboard">
+        <div className="my-5 mx-auto">
+           {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+            <InfoCard
+              icon={<IoMdCard />}
+              label="Total Balance"
+              value={addThousandsSeperator(dashboardData?.totalBalance || 0)}
+              color="bg-primary"
+            />
+
+            <InfoCard
+              icon={<LuWalletMinimal />}
+              label="Total Income"
+              value={addThousandsSeperator(dashboardData?.totalIncome || 0)}
+              color="bg-orange-500"
+            />
+
+            <InfoCard
+              icon={<LuHandCoins />}
+              label="Total Expense"
+              value={addThousandsSeperator(dashboardData?.totalExpense || 0)}
+              color="bg-red-500"
+            />
+          </div> 8 */}
+
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+            <RecentTransactions
+            transactions={dashboardData?.RecentTransactions}
+            onSeeMore={() => navigate("/expense")}
+            />
+          </div>
+        </div>
+      </DashboardLayout>
+    );
   };
 
-  useEffect(() => {
-    fetchDashboardData();
-    return () => {};
-  }, []);
-
-  return (
-    <DashboardLayout activeMenu="Dashboard">
-      <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <InfoCard
-            icon={<IoMdCard />}
-            label="Total Balance"
-            value={addThousandsSeperator(dashboardData?.totalBalance || 0)}
-            color="bg-primary"
-          />
-        </div>
-      </div>
-    </DashboardLayout>
-  );
-};
-
-export default Home;
+  export default Home;
